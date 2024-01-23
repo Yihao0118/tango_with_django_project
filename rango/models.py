1ca63e887d0d82c43bfaf395cf8a8dac58e36e64
Yihao0118 <2746098c@student.gla.ac.uk>
2024-01-23 22:57:45 +0000

    chapter 5 23.1.2024

--- rango/models.py
+++ rango/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 class Category(models.Model):
@@ -19,3 +20,6 @@ class Page(models.Model):
     
     def __str__(self):
         return self.title
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')

d284c9337cedd395955aa474aea7cc5361fcd1b4
Yihao0118 <2746098c@student.gla.ac.uk>
2024-01-25 17:15:05 +0000

    chapter 7 25.1.2024

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,47 @@
 from django.shortcuts import render
+from rango.forms import CategoryForm
+from rango.forms import PageForm
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from rango.models import Category
+from django.urls import reverse
 from rango.models import Page
+
+def add_category(request):
+    form=CategoryForm()
+    if request.method=='POST':
+        form=CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+    return render(request,'rango/add_category.html',{'form':form})
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+
+    form = PageForm()
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                # probably better to use a redirect here.
+            return show_category(request, category_name_slug)
+        else:
+            print(form.errors)
+
+    context_dict = {'form':form, 'category': category}
+
+    return render(request, 'rango/add_page.html', context_dict)
+
 
 def show_category(request, category_name_slug):
     context_dict = {}
